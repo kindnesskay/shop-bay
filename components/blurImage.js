449252b16d116754e488c713f2b97aba5476050e
kindesskay <kindenessudubamo@gmail.com>
2024-01-01 00:30:00 +0100

    ui update

--- components/blurImage.js
+++ components/blurImage.js
@@ -1,29 +1,20 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-export default function BlurImage({ image, height, width, alt,onClick }) {
+export default function BlurImage({ image, height, width, alt, onClick }) {
   const [isLoading, setLoading] = useState(true);
-  
- 
+
   return (
     <div className="w-full flex justify-center overflow-hidden rounded-lg">
       <Image
-      onClick={onClick}
+        onClick={onClick}
         alt={alt || "image"}
         src={image || "/blur.png"}
         height={height || 200}
         width={width || 200}
-        loading="lazy"
-        className={`
-              duration-700 ease-in-out group-hover:opacity-75 h-auto w-auto max-h-full
-              ${
-                isLoading
-                  ? "scale-110 blur-2xl grayscale"
-                  : "scale-100 blur-0 grayscale-0"
-              })`}
-        onLoad={() => setLoading(false)}
+        priority
+        className={`h-auto w-auto max-h-full`}
       />
-
     </div>
   );
 }
